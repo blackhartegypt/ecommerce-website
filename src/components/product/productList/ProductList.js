3d415fb723e1e68c652241d09fbc732683eb51dd
blackhartegypt@gmail.com <blackhartegypt@gmail.com>
2024-10-20 13:47:58 +0300

    added productList.js

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,11 +1,95 @@
 import styles from './ProductList.module.scss';
+import { useEffect,useState } from 'react';
+import { BsFillGridFill } from 'react-icons/bs';
+import {FaListAlt} from "react-icons/fa";
+import Search from "../../search/Search";
 import ProductItem from '../productItem/ProductItem';
+import {useDispatch,useSelector} from "react-redux";
+import {
+  FILTER_BY_SEARCH,selectFilteredProducts,SORT_PRODUCTS
+} from "../../../redux/slices/filterSlice";
+import Pagination from "../../pagination/Pagination";
 
-import React from 'react'
 
-const ProductList = () => {
+
+const ProductList = ({products}) => {
+
+  const [grid,setGrid]=useState(true);
+  const [search,setSearch]=useState(true);
+  const [sort,setSort]=useState("latest");
+  const filteredProducts=useSelector(selectFilteredProducts);
+
+  const [currentPage,setCurrentPage]=useState(1);
+  const [productsPerPage]=useState(9);
+  const indexOfLastProduct=currentPage*productsPerPage;
+  const indexOfFistProduct=indexOfLastProduct-productsPerPage;
+  const currentProducts=filteredProducts.slice(indexOfFistProduct,indexOfLastProduct);
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    dispatch(SORT_PRODUCTS({
+      products,
+      sort
+    }));
+  },[dispatch,products,sort])
+
+  useEffect(()=>{
+    dispatch(FILTER_BY_SEARCH({
+       products,
+       search
+    }));
+  },[dispatch,search,products])
+
+
   return (
-    <div>ProductList</div>
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icons}>
+          <BsFillGridFill size={22} color="orangered" onClick={()=>setGrid(false)}/>
+          <FaListAlt size={24} color="#0066d4" onClick={()=>setGrid(false)} />
+          <p>
+            <b>{filteredProducts.length}</b> ProductsFound
+          </p>
+        </div>
+        <div>
+          <Search value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        </div>
+
+        <div className={styles.sort}>
+          <label>Sort By:</label>
+            <select value={sort} onChange={(e)=>setSort(e.target.value)}>
+              <option value="latest">Latest</option>
+              <option value="lowest-price">Lowest Price</option>
+              <option value="highest-price">Highest Price</option>
+              <option value="a-z">A - Z</option>
+              <option value="z-a">Z - A</option>
+            </select>
+        </div>
+      </div>
+
+      <div className={grid ? `${styles.grid}`:styles.list}>
+        {products.length === 0 ? (
+          <p>No Product Found in That Category.</p>
+        ):(
+          <>
+          {currentProducts.map(product =>{
+            return(
+              <div key={product.id}>
+                <ProductItem {...product} grid={grid} product={product}/>
+              </div>
+            )
+          })}
+          </>
+        )}
+
+        <Pagination 
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          productsPerPage={productsPerPage}
+          totalProducts={filteredProducts.length}
+        />  
+      </div>
+    </div>
   )
 }
 
