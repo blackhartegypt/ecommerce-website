cbe1f426d55b25431f920cd72eaa68d32d0fa62d
blackhartegypt@gmail.com <blackhartegypt@gmail.com>
2024-10-22 17:58:17 +0300

    add product review component

--- src/components/reviewProducts/ReviewProducts.js
+++ src/components/reviewProducts/ReviewProducts.js
@@ -1,12 +1,99 @@
 
-
 import styles from './ReviewProducts.module.scss'
+import {useState,useEffect} from "react";
+import {useSelector} from "react-redux";
+import Card from "../../Card";
+import styles from "./ReviewProducts.module.scss";
+import StarsRating from "react-star-rate";
+import {addDoc,collection,Timestamp} from "firebase/firestore";
+import { db } from "../../firebase/config";
+import { toast } from 'react-toastify';
+import useFetchDocument from "../../customHooks/useFetchDocument";
+import spinnerImg from "../../assets/spinner.jpg";
 
-import React from 'react'
+
 
 const ReviewProducts = () => {
+  const [rate, setRate] = useState(0);
+  const [review, setReview] = useState("");
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  const { document } = useFetchDocument("products", id);
+  const userID = useSelector(selectUserID);
+  const userName = useSelector(selectUserName);
+
+  useEffect(()=>{
+    setProduct(document);
+  },[document]);
+
+  const submitReview=(e)=>{
+    e.preventDefault();
+    const date=new Date().toDateString();
+
+    const reviewConfig={
+      userID,
+      userName,
+      ProductID:id,
+      rate,
+      review,
+      reviewDate:date,
+      createdAt:Timestamp.now().toDate(),
+    };
+
+    try {
+      addDoc(collection(db,"reviews"),reviewConfig);
+      toast.success("Your review has been successfully Submited");
+      setRate(0);
+      setReview("");
+    } catch (err){
+      toast.error(err.message)
+    }
+
+  }
+
   return (
-    <div>ReviewProducts</div>
+    <section>
+      <div className={`container ${styles.review}`}>
+        <h2>Review Products</h2>
+        {product === null ? (
+          <img src={spinnerImg} alt="Loading..." style={{ width: "50px" }} />
+        ) : (
+          <>
+            <p>
+              <b>Product name:</b> {product.name}
+            </p>
+            <img
+              src={product.imageURL}
+              alt={product.name}
+              style={{ width: "100px" }}
+            />
+          </>
+        )}
+
+        <Card cardClass={styles.card}>
+          <form onSubmit={(e) => submitReview(e)}>
+            <label>Rating:</label>
+            <StarsRating
+              value={rate}
+              onChange={(rate) => {
+                setRate(rate);
+              }}
+            />
+            <label>Review</label>
+            <textarea
+              value={review}
+              required
+              onChange={(e) => setReview(e.target.value)}
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button type="submit" className="--btn --btn-primary">
+              Submit Review
+            </button>
+          </form>
+        </Card>
+      </div>
+    </section>
   )
 }
 
