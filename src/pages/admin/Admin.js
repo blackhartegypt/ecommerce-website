6bfa95f11909a627e15e766ca4a01182c9c5b702
blackhartegypt@gmail.com <blackhartegypt@gmail.com>
2024-11-03 18:17:45 +0300

    add admin page component

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,12 +1,28 @@
 import styles from './Admin.module.scss';
+import {Route,Routes} from "react-router-dom";
+import AddProduct from "../../components/admin/addProduct/AddProduct";
+import Home from "../../components/admin/home/Home";
+import Navbar from "../../components/admin/navbar/Navbar";
+import OrderDetails from "../../components/admin/orderDetails/OrderDetails";
+import Orders from "../../components/admin/orders/Orders";
+import ViewProducts from "../../components/admin/viewProducts/ViewProducts";
 
-const Admin=()=>{
-
-
-    return(
- <h2 className={styles.h2}> This is going to be fun guys </h2>
-
-    )
-}
-
+const Admin = () => {
+    return (
+      <div className={styles.admin}>
+        <div className={styles.navbar}>
+          <Navbar />
+        </div>
+        <div className={styles.content}>
+          <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="all-products" element={<ViewProducts />} />
+            <Route path="add-product/:id" element={<AddProduct />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="order-details/:id" element={<OrderDetails />} />
+          </Routes>
+        </div>
+      </div>
+    );
+  };
 export default Admin
