40b5216dda553fe52e531b021fed3992e3d55fec
blackhartegypt@gmail.com <blackhartegypt@gmail.com>
2024-11-11 15:22:30 +0300

    stored firebase configurations in environment variables and rolled new keys

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,25 +2,22 @@ import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
-
+require("dotenv").config();
 
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyBFZx4t7jE2LzsgLUDzdkKsjhMEJcYzojw",
-    authDomain: "eweb-50135.firebaseapp.com",
-    projectId: "eweb-50135",
-    storageBucket: "eweb-50135.appspot.com",
-    messagingSenderId: "40693679741",
-    appId: "1:40693679741:web:78843fef64cdef476a5314",
-    measurementId: "G-HRQMNXGSGM"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
   };
-
 
   const app=initializeApp(firebaseConfig);
   export const db=getFirestore(app);
   export const storage=getStorage(app);
   export const auth=getAuth(app);
 
-//since you can access the instance of the app in all the other modules and code section console the options and after integrating the code
-// in other components console.log(app.options)
   export default app;
